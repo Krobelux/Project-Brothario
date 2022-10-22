38f32dee64fb41e9f4aeaedd0648d44f027cae2f
Jacob T <94589127+Krobelux@users.noreply.github.com>
2022-10-22 11:50:19 -0400

    Main Menu
    Added a Main Menu
    - Found BG images
    - Layered them and animated them
    - Added functionality to the Main Menu buttons

---
+++ Assets/Scripts/BGScrolling.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScrolling : MonoBehaviour
+{
+
+    public float scrollSpeed = 0.01f;
+
+    private Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.mainTextureOffset = new Vector2(Time.time * scrollSpeed, 0);
+    }
+}
